92497c190213c7a689efa7348c4aa930ac8483a2
Mark Steve Samson <contact@marksteve.me>
2011-11-11 01:51:46 +0800

    Use Heroku's PostgreSQL

--- bookmarks.py
+++ bookmarks.py
@@ -5,7 +5,7 @@ import os
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bookmarks.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
 
